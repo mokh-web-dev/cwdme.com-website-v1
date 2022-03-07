53c20d5dc65c27388e936318746b7f7368da0df3
mokh-web-dev <mokh.web.dev@gmail.com>
2022-03-07 13:33:29 +0400

    Added email functinoality to contact us form.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Typed text effect on Hero variables
+
 const typedTextSpan = document.querySelector('.typed-text');
 const cursorSpan = document.querySelector('.cursor');
 
@@ -13,6 +15,15 @@ const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
+
+// Contact Us form cariables
+
+const btnHere = document.querySelector('.contact-us__p--here');
+const popupWindow = document.querySelector('.popup');
+const btnHereClose = document.querySelector('.popup__close');
+const overlay = document.querySelector('.overlay');
+const submitBtn = document.querySelector('.submit__btn');
+const contactUsForm = document.querySelector('.form');
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
@@ -59,12 +70,6 @@ window.addEventListener('scroll', function () {
   parallax.style.backgroundPositionY = offset * 0.7 + 'px';
 });
 
-const btnHere = document.querySelector('.contact-us__p--here');
-const popupWindow = document.querySelector('.popup');
-const btnHereClose = document.querySelector('.popup__close');
-const overlay = document.querySelector('.overlay');
-const submitBtn = document.querySelector('.submit__btn');
-
 const toggleWindow = function () {
   overlay.classList.toggle('hidden');
   popupWindow.classList.toggle('hidden');
@@ -82,6 +87,28 @@ overlay.addEventListener('click', function () {
   toggleWindow();
 });
 
-submitBtn.addEventListener('click', function () {
-  console.log(`Submit button clicked`);
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+
+  sendMessage(name, email, message);
+  contactUsForm.reset();
+  toggleWindow();
 });
+
+const sendMessage = function (name, email, message) {
+  Email.send({
+    SecureToken: '3617a64d-b088-4e04-8712-c058397a5214',
+    To: 'info@cwdme.com',
+    From: 'info@cwdme.com',
+    Subject: `${name} sent you a message`,
+    Body: `Name: ${name}<br><br>Email: ${email}<br><br>Message: ${message}`,
+  }).then((message) =>
+    alert(
+      'Your message has been sent sucessfully! We will get in touch with you shortly.'
+    )
+  );
+};
